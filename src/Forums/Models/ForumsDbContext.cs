e15064f90817ab88af61da3377f37808f2090658
Russ Davies <russdavies392@gmail.com>
2016-09-23 19:37:13 -0700

    add ForumsDbContext model

---
+++ src/Forums/Models/ForumsDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Forums.Models
+{
+    public class ForumsDbContext : DbContext
+    {
+        public DbSet<Topic> Topics { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public ForumsDbContext(DbContextOptions<ForumsDbContext> options)
+        
+            : base(options)
+        { 
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}

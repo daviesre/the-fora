7768121af931f50ae9c6f6bea132ae4679d2ff11
Russ Davies <russdavies392@gmail.com>
2016-09-27 17:07:24 -0700

    add ability to see topic name in post area

--- src/Forums/Controllers/PostsController.cs
+++ src/Forums/Controllers/PostsController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Forums.Models;
 using Microsoft.EntityFrameworkCore;
+using Forums.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -16,7 +17,10 @@ namespace Forums.Controllers
         private ForumsDbContext db = new ForumsDbContext();
         public IActionResult Index(int id)
         {
-            return View(db.Posts.Include(posts => posts.Topic).Where(topics => topics.TopicId == id).ToList());
+            var model = new PostsViewModel();
+            model.topic = db.Topics.FirstOrDefault(topic => topic.TopicId == id);
+            model.Posts = db.Posts.Include(posts => posts.Topic).Where(topics => topics.TopicId == id).ToList();
+            return View(model);
         }
 
         public IActionResult Create()
---
+++ src/Forums/ViewModels/PostsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Forums.Models;
+
+namespace Forums.ViewModels
+{
+    public class PostsViewModel
+    {
+        public Topic topic { get; set; }
+        public IEnumerable<Post> Posts { get; set; }
+    }
+}

9062877b6d45222171b6abb8742252871dbe19c0
Russ Davies <russdavies392@gmail.com>
2016-09-29 11:10:25 -0700

    add testing for successful return of HomeController view

---
+++ Forums.Tests/ControllerTests/TopicsControllerTest.cs
@@ -0,0 +1,24 @@
+﻿using Forums.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Forums.Tests.ControllerTests
+{
+    public class TopicsControllerTest
+    {
+        [Fact]
+        public void Get_ViewResult_Index_Test()
+        {
+            //Arrange
+            HomeController controller = new HomeController();
+            //Act
+            var result = controller.Index();
+            //Assert
+            Assert.IsType<ViewResult>(result);
+      }
+    }
+}

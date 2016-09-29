c347f876dd055ba571d5b7833dfabc96a61bcb04
Russ Davies <russdavies392@gmail.com>
2016-09-29 11:28:20 -0700

    test rest of the models to see if they produce correct properties

---
+++ Forums.Tests/ModelTests/CommentTest.cs
@@ -0,0 +1,24 @@
+﻿using Forums.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Forums.Tests.ModelTests
+{
+    public class CommentTest
+    {
+        [Fact]
+        public void GetTextTest()
+        {
+            //Arrange
+            var comment = new Comment();
+            comment.Text = "I don't know";
+            //Act
+            var result = comment.Text;
+            //Assert
+            Assert.Equal("I don't know", result);
+        }
+    }
+}
---
+++ Forums.Tests/ModelTests/PostTest.cs
@@ -0,0 +1,24 @@
+﻿using Forums.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Forums.Tests.ModelTests
+{
+    public class PostTest
+    {
+        [Fact]
+        public void GetTitleTest()
+        {
+            //Arrange
+            var post = new Post();
+            post.Title = "Where should I go to eat?";
+            //Act
+            var result = post.Title;
+            //Assert
+            Assert.Equal("Where should I go to eat?", result);
+        }
+    }
+}
